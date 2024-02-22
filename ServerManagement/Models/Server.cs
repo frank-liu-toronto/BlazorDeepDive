203a4ee18beb2936231782e0cac01d213d534663
Frank Liu <liu1000@gmail.com>
2024-02-22 12:33:14 -0500

    Razor syntax

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber == 0? false : true;
+        }
+
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+    }
+}

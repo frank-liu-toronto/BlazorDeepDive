ee3c808eda4239109f170aece009b12b38af6fbf
Frank Liu <liu1000@gmail.com>
2024-02-22 17:02:44 -0500

    Output City buttons

---
+++ ServerManagement/Models/CitiesRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagement.Models
+{
+    public static class CitiesRepository
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => cities;
+        
+    }
+}

ea1ee38bad2d5eac41860fffa5f3a98cfe6a2f18
Frank Liu <liu1000@gmail.com>
2024-03-08 10:17:12 -0500

    Enable Server Interactivity: how to make a component interactive

--- ServerManagement/Program.cs
+++ ServerManagement/Program.cs
@@ -3,7 +3,8 @@ using ServerManagement.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -20,6 +21,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>().
+    AddInteractiveServerRenderMode();
 
 app.Run();

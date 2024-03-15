51009133214ee6d57423da5dd4e2daa1301ec9f4
Frank Liu <liu1000@gmail.com>
2024-03-15 18:13:38 -0400

    Mark task as completed use case

--- ToDoApp/Models/ToDoItem.cs
+++ ToDoApp/Models/ToDoItem.cs
@@ -5,7 +5,20 @@
         public int Id { get; set; }
         public string Name { get; set; } = "";
 
-        public bool IsCompleted { get; set; }
+        private bool _isCompleted;
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set
+            {
+                _isCompleted = value;
+
+                if (value)
+                {
+                    DateCompleted = DateTime.Now;
+                }
+            }
+        }
 
         public DateTime DateCompleted { get; set; }
     }

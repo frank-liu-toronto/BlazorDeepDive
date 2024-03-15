af3b9203c7fc88842370b31c03a28ada5932df5e
Frank Liu <liu1000@gmail.com>
2024-03-15 17:32:19 -0400

    Implement Display a list of tasks use case

---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime DateCompleted { get; set; }
+    }
+}
---
+++ ToDoApp/Models/ToDoItemsRepository.cs
@@ -0,0 +1,78 @@
+﻿
+namespace ToDoApp.Models
+{
+    public static class ToDoItemsRepository
+    {
+        private static List<ToDoItem> items = new List<ToDoItem>()
+        {
+            new ToDoItem { Id = 1, Name = "Item 1" },
+            new ToDoItem { Id = 2, Name = "Item 2" },
+            new ToDoItem { Id = 3, Name = "Item 3" },
+            new ToDoItem { Id = 4, Name = "Item 4" },
+        };
+
+        public static void AddItem(ToDoItem item)
+        {
+            if (items.Count > 0)
+            {
+                var maxId = items.Max(s => s.Id);
+                item.Id = maxId + 1;
+                items.Add(item);
+            }
+            else
+            {
+                item.Id = 1;
+                items.Add(item);
+            }
+
+        }
+
+        public static List<ToDoItem> GetItems()
+        {
+            var sortedItems = items.
+                OrderBy(item => item.IsCompleted)
+                .ThenByDescending(item => item.Id)
+                .ToList();
+            return sortedItems;
+        }
+
+        public static ToDoItem? GetItemById(int id)
+        {
+            var item = items.FirstOrDefault(s => s.Id == id);
+            if (item != null)
+            {
+                return new ToDoItem
+                {
+                    Id = item.Id,
+                    Name = item.Name,                    
+                };
+            }
+            return null;
+        }
+
+        public static void UpdateItem(int itemId, ToDoItem item)
+        {
+            if (itemId != item.Id) return;
+
+            var itemToUpdate = items.FirstOrDefault(s => s.Id == itemId);
+            if (itemToUpdate != null)
+            {                
+                itemToUpdate.Name = item.Name;             
+            }
+        }
+
+        public static void DeleteItem(int itemId)
+        {
+            var item = items.FirstOrDefault(s => s.Id == itemId);
+            if (item != null)
+            {
+                items.Remove(item);
+            }
+        }
+
+        public static List<ToDoItem> SearchItems(string itemFilter)
+        {
+            return items.Where(s => s.Name.Contains(itemFilter, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+    }
+}

24b5c9ca73a768111eddf3bdc91d23760ed537bc
Frank Liu <liu1000@gmail.com>
2024-03-29 17:56:49 -0400

    The problem of component initialization with Server Interactivity

---
+++ ServerManagement/Components/Controls/BaseComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ServerManagement.Components.Controls
+{
+    public class BaseComponent : ComponentBase
+    {
+        protected bool isFirstRender { get; set; } = true;
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+
+            isFirstRender = firstRender;
+        }
+
+    }
+}

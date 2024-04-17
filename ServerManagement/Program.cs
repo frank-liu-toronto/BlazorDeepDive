eda86ac537ecb6e8c266b7401b0eba7df7902476
Frank Liu <liu1000@gmail.com>
2024-04-16 21:47:11 -0400

    Protective storage

--- ServerManagement/Program.cs
+++ ServerManagement/Program.cs
@@ -1,10 +1,13 @@
 using ServerManagement.Components;
+using ServerManagement.StateStore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddTransient<SessionStorage>();
 
 var app = builder.Build();
 
---
+++ ServerManagement/StateStore/SessionStorage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using ServerManagement.Models;
+
+namespace ServerManagement.StateStore
+{
+    public class SessionStorage
+    {
+        private readonly ProtectedSessionStorage protectedSessionStorage;
+
+        public SessionStorage(ProtectedSessionStorage protectedSessionStorage)
+        {
+            this.protectedSessionStorage = protectedSessionStorage;
+        }
+
+        public async Task<Server?> GetServerAsync()
+        {
+            var result = await this.protectedSessionStorage.GetAsync<Server>("server");
+            if (result.Success)
+                return result.Value;
+            else 
+                return null;
+        }
+
+        public async Task SetServerAsync(Server? server)
+        {
+            await this.protectedSessionStorage.SetAsync("server", server);
+        }
+    }
+}
